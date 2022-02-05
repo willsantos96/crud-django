df224d410ab12f04ae802d20d631a764685ae0f6
willsantos96 <willsantos96@gmail.com>
2022-02-04 22:16:54 -0300

    adicionando mensagens de erro

--- contatos/views.py
+++ contatos/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import get_object_or_404, render, get_list_or_404
+from django.shortcuts import get_object_or_404, render, get_list_or_404, redirect
 from django.http import Http404
 from .models import Contato
 from django.core.paginator import Paginator
 from django.db.models import Q, Value
 from django.db.models.functions import Concat
+from django.contrib import messages
 
 
 def index(request):
@@ -36,7 +37,10 @@ def busca(request):
     termo = request.GET.get('termo')
 
     if termo is None or not termo:
-        raise Http404()
+        messages.add_message(request, messages.ERROR,
+                             'Campo termo não pode ficar vazio')
+
+        return redirect('index')
 
     campos = Concat('nome', Value(' '), 'sobrenome')
 
